61aa2dff18b27034de73242d3e0b773c9613fb22
João Pedro Kuhn <pedrojoaojpk@gmail.com>
2024-11-19 18:30:09 +0000

    correções dos agentes

--- agentes.py
+++ agentes.py
@@ -27,6 +27,7 @@ docs_scrape_tool = ScrapeWebsiteTool(
         "https://www.gov.br/anatel/pt-br/consumidor/conheca-seus-direitos/telefonia-movel",
         "https://www.gov.br/anatel/pt-br/consumidor/conheca-seus-direitos/telefonia-fixa",
         "https://www.gov.br/anatel/pt-br/consumidor/conheca-seus-direitos/tv-por-assinatura",
+        "https://www.gov.br/anatel/pt-br/consumidor/perguntas-frequentes"
     ]
 )
 docs_search_tool = WebsiteSearchTool(website_url="https://www.gov.br/anatel/pt-br/consumidor/perguntas-frequentes")
@@ -40,36 +41,37 @@ pdf_search4 = PDFSearchTool(pdf="normas/Anatel - Resolução nº 581, de 26 de m
 pdf_search5 = PDFSearchTool(pdf="normas/Anatel - Resolução nº 632, de 7 de março de 2014.pdf")
 pdf_search6 = PDFSearchTool(pdf="normas/oficio_12273325.pdf")
 
-#Definição dos 3 agentes:
+#Definição dos 4 agentes:
 identificador = Agent(
     role="Identificador de Problemas",
     goal="Receber a dúvida do cliente e identificar de qual área é o seguinte problema: {problema}",
-    backstory="Você está trabalhando na identificação da área do problema do cliente "
+    backstory="Você está trabalhando na identificação de um problema de telecomunicações"
               "sobre o problema: {problema}."
               "Você coleta informações atualizadas através de sites e arquivos que ajudam a "
               "identificar a área do problema "
-              "e indica qual é o agente especializado no problema "
+              "e indica qual é o agente especializado no problema, caracterizando-o em jurídico, se for mais relacionado a alguma norma,"
+              "ou técnico, se está relacionado com o uso de algum equipamento. "
               "As únicas duas áreas de problemas principais são Jurídico e Técnico "
               "Seu trabalho é a base para que o "
-              "agente seguinte escreva possiveis soluções para o problema."
+              "agente seguinte escreva possíveis soluções para o problema."
               "O próximo agente pode ser o Solucionador de problemas da área Jurídica ou o Solucionador de problemas da área Técnica ",
     verbose=True,
-    tools=[search_tool, scrape_tool, docs_scrape_tool, docs_search_tool,pdf_search, pdf_search1, pdf_search2, pdf_search3, pdf_search4, pdf_search5, pdf_search6, texto_search],
+    tools=[search_tool, scrape_tool, docs_scrape_tool, docs_search_tool, pdf_search, pdf_search1, pdf_search2, pdf_search3, pdf_search4, pdf_search5, pdf_search6, texto_search],
     allow_delegation=False,
     llm=gpt4o_mini_llm
 )
 
 juridico = Agent(
     role="Solucionador de problemas da área Jurídica",
-    goal="Definir uma solução, com base nas leis da Anatel, para o cliente a fim de resolver seu questionamento: {problema}",
+    goal="Definir uma solução, com base nas leis da Anatel, nas perguntas frequentes da Anatel e no arquivo txt resposta_exemplar, para o cliente a fim de resolver seu questionamento: {problema}",
     backstory="Você é um solucionador que recebe um problema de um cliente. "
-              "Seu objetivo é dar soluções jurídicas para o problema do cliente."
+              "Seu objetivo é dar soluções jurídicas para o problema do cliente, baseando-se nas normas da Anatel."
               "Você também fornece insights objetivos e imparciais "
               "e os apoia com as informações "
               "baseadas nas leis da Anatel e regulamentações da Anatel. "
-              "Após análise concluída você escreve um artigo aconselhando o cliente para a solução do seu problema."
-              "Envie esse artigo para o Supervisor de artigos"
-              "Caso o problema não seja da sua área não escreva nada no documento",
+              "Após análise concluída você escreve um pequeno texto, semelhante às respostas da Anatel para perguntas frequentes, aconselhando o cliente para a solução do seu problema."
+              "Envie esse texto para o Supervisor de artigos"
+              "Caso o problema não tenha nenhuma relação com telecomunicações ou com Anatel, retorne apenas um comentário dizendo que você não é capaz de resolver seu problema.",
     verbose=True,
     tools=[pdf_search, pdf_search1, pdf_search2, pdf_search3, pdf_search4, pdf_search5, pdf_search6, texto_search, docs_search_tool],
     allow_delegation=False,
@@ -80,14 +82,14 @@ juridico = Agent(
 tecnico = Agent(
     role="Solucionador de problemas da área Técnica",
     goal="Definir uma solução, com base em datasheets sobre o produto ou equipamento definidos pelo cliente para solução do problema: {problema}",
-    backstory="Você é um solucionador que recebe um problema de um cliente. "
+    backstory="Você é um solucionador que recebe um problema de um cliente de telecomunicações, podendo este ser um técnico ou um engenheiro. "
               "Seu objetivo é dar soluções técnicas para o problema do cliente."
               "Você também fornece insights objetivos e imparciais "
               "e os apoia com as informações "
               "fornecidas pelos datasheets. "
-              "Após análise concluída você escreve um artigo aconselhando o cliente para a solução do seu problema."
-              "Envie esse artigo para o Supervisor de artigos"
-              "Caso o problema não seja da sua área não escreva nada no documento",
+              "Após análise concluída você escreve um pequeno texto, semelhante às respostas da Anatel para perguntas frequentes, aconselhando o cliente para a solução do seu problema."
+              "Envie esse texto para o Supervisor de artigos"
+              "Caso o problema não tenha nenhuma relação com telecomunicações ou com Anatel, retorne apenas um comentário dizendo que você não é capaz de resolver seu problema.",
     verbose=True,
     tools=[pdf_search, pdf_search1, pdf_search2, pdf_search3, pdf_search4, pdf_search5, pdf_search6, texto_search, docs_search_tool],
     allow_delegation=False,
@@ -97,11 +99,8 @@ tecnico = Agent(
 supervisor = Agent(
     role="Supervisor de artigos",
     goal="Analisar e corrigir possíveis erros cometidos pelo agente jurídico e pelo técnico para solucionar o seguinte problema: {problema}",
-    backstory="Você é um supervisor de artigos que recebe um artigo com soluções para o problema: {problema}."  
-    
-    
-    
-              "Seu objetivo é supervisar os artigos produzidos pelos agentes jurídicos e técnicos, corrigindo erros jurídicos, técnicos e gramaticais da lígua portuguesa."
+    backstory="Você é um supervisor de artigos que recebe um texto com soluções para o problema: {problema}."  
+              "Seu objetivo é supervisar os textos produzidos pelos agentes jurídicos e técnicos, corrigindo erros jurídicos, técnicos e gramaticais da lígua portuguesa e também que ela seja conciso na resposta para o cliente, baseando nas respostas da Anatel para as perguntas frequentes ."
               "Você deve verificar se as soluções são coerentes com as leis da Anatel, com os datasheets utilizados para consulta e usar o modelo resposta_exemplar.txt."
               "Caso o problema não seja da área da Telecomunicações, apague o texto do solucionador de problemas e escreva que o problema está fora do escopo"
               "Por fim, retorne o documento corrigido, resumido e com as referências da pesquisa ao final.",
